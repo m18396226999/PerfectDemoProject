5790bd09f602170085b70d63c4fe90d72da1a633
许东晓 <xudongxiao@xudongxiaodeMacBook-Air-3.local>
2018-11-02 14:59:12 +0800

    4

---
+++ Sources/main.swift
@@ -0,0 +1,34 @@
+//
+//  main.swift
+//  PerfectTemplate
+//
+//  Created by Kyle Jessup on 2015-11-05.
+//    Copyright (C) 2015 PerfectlySoft, Inc.
+//
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Perfect.org open source project
+//
+// Copyright (c) 2015 - 2016 PerfectlySoft Inc. and the Perfect project authors
+// Licensed under Apache License v2.0
+//
+// See http://perfect.org/licensing.html for license information
+//
+//===----------------------------------------------------------------------===//
+//
+
+import PerfectLib
+import PerfectHTTP
+import PerfectHTTPServer
+
+//HTTP服务
+var routesArr = [Dictionary<String, Any>]()
+
+var someDict1 : [String:String] = ["method":"GET","url":"/api"]
+
+routesArr.append(someDict1)
+
+let networkServer = NetworkServerManager(root: "webroot", port: 8080)
+
+networkServer.startServer()
+
